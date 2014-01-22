cbdf3dd1a967b38fc3a0988596497ffa64e53dd8
Zack Liscio <zackliscio@gmail.com>
2014-01-22 12:53:37 -0500

    omniauth linkedin

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem 'pg'
 gem 'simple_form'
 gem 'unicorn'
 gem 'unicorn-rails'
+gem 'omniauth'
+gem 'omniauth-linkedin'
 group :development do
   gem 'better_errors'
   gem 'binding_of_caller', :platforms=>[:mri_19, :mri_20, :rbx]
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Leadgen::Application.routes.draw do
   root :to => "home#index"
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  # devise_scope :user do
+  #  get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
+  get '/auth/:provider/callback' => 'sessions#create'
+  get '/signout' => 'sessions#destroy', :as => :signout
+  get '/auth/failure' => 'sessions#failure'
 end
